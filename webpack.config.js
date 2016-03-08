e42d1dfeed983bb98b919c67086f841895132438
yowcow <yowcow@cpan.org>
2016-03-08 14:35:48 +0000

    add babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,31 @@
 var WebPack = require('webpack');
 
 module.exports = {
-    entry: {
-        app: './src/app.js',
-    },
-    output: {
-        path:              __dirname,
-        filename:          "[name].js",
-        sourceMapFilename: "[name].map"
-    },
-    module: {
-        loaders: [
-            { test: /\.css$/, loader: 'style!css' }
-        ]
-    },
-    plugins: [
-        new WebPack.optimize.UglifyJsPlugin()
+  entry: {
+    app: './src/app.js',
+  },
+  output: {
+    path:              __dirname,
+    filename:          "[name].js",
+    sourceMapFilename: "[name].map"
+  },
+  module: {
+    loaders: [
+      {
+        test:   /\.css$/,
+        loader: 'style!css'
+      },
+      {
+        test:    /\.es6$/,
+        exclude: /node_modules/,
+        loader:  'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
     ]
+  },
+  plugins: [
+    new WebPack.optimize.UglifyJsPlugin()
+  ]
 };
