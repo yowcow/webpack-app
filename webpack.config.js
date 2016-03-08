1eb4dc285ffcc824093b34e0f37f1544a2f2ad15
yowcow <yowcow@cpan.org>
2016-03-08 10:15:19 +0000

    entry

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
+var WebPack = require('webpack');
+
 module.exports = {
+    entry: {
+        app: './src/app.js',
+    },
     output: {
-        path: __dirname,
-        filename: "bundle.js"
+        path:              __dirname,
+        filename:          "[name].js",
+        sourceMapFilename: "[name].map"
     },
     module: {
         loaders: [
             { test: /\.css$/, loader: 'style!css' }
         ]
-    }
+    },
+    plugins: [
+        new WebPack.optimize.UglifyJsPlugin()
+    ]
 };
