9960272177804164a699547776f67f2671484a6d
yowcow <yowcow@cpan.org>
2016-03-08 14:35:17 +0000

    webpack to do es6->js conversion

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,7 @@
 var gulp    = require('gulp');
 var webpack = require('gulp-webpack');
-var babel   = require('gulp-babel');
 var mocha   = require('gulp-mocha');
 var babel_compiler = require('babel-core/register');
-
-gulp.task('babel', function () {
-    gulp.src('./src/*.es6')
-        .pipe(babel())
-        .pipe(gulp.dest('./src/'));
-});
 
 gulp.task('webpack', function () {
     gulp.src('./src/*.js')
@@ -27,8 +20,7 @@ gulp.task('mocha', function () {
 
 gulp.task('watch', function () {
     gulp.watch('src/*.js', ['webpack']);
-    gulp.watch('src/*.es6', ['babel', 'mocha']);
     gulp.watch('test/*.es6', ['mocha']);
 });
 
-gulp.task('default', ['mocha', 'babel', 'webpack', 'watch']);
+gulp.task('default', ['mocha', 'webpack', 'watch']);
