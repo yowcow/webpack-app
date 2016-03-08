fc8ba24dfef10c08f49ffec6a5292b327609c106
yowcow <yowcow@cpan.org>
2016-03-08 11:01:23 +0000

    tutorial

--- src/app.es6
+++ src/app.es6
@@ -1,1 +1,69 @@
-alert('hoge')
+var React    = require('react')
+var ReactDOM = require('react-dom')
+var marked   = require('marked')
+
+var CommentForm = React.createClass({
+    render() {
+        return (
+            <div className="commentForm">
+                Hello, world! I am a CommentForm.
+            </div>
+        )
+    }
+})
+
+var Comment = React.createClass({
+    render() {
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">
+                    {this.props.author}
+                </h2>
+                <span dangerouslySetInnerHTML={this.rawMarkup()} />
+            </div>
+        )
+    },
+    rawMarkup() {
+        var raw = marked(this.props.children.toString(), {sanitize: true})
+        return { __html: raw }
+    }
+})
+
+var CommentList = React.createClass({
+    render() {
+        var commentNodes = this.props.data.map(comment => {
+            return (
+                <Comment author={comment.author} key={comment.id}>
+                    {comment.text}
+                </Comment>
+            )
+        })
+        return (
+            <div className="commentList">
+                {commentNodes}
+            </div>
+        )
+    }
+})
+
+var CommentBox = React.createClass({
+    render() {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList data={this.props.data} />
+                <CommentForm />
+            </div>
+        )
+    }
+})
+
+var data = [
+    { id: 1, author: "Hoge", text: "あああああ" },
+    { id: 2, author: "Fuga", text: "いいいいい" }
+]
+
+ReactDOM.render(
+    <CommentBox data={data} />,
+    document.getElementById('content')
+)
--- src/app.js
+++ src/app.js
@@ -1,3 +1,75 @@
 'use strict';
 
-alert('hoge');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var marked = require('marked');
+
+var CommentForm = React.createClass({
+    displayName: 'CommentForm',
+    render: function render() {
+        return React.createElement(
+            'div',
+            { className: 'commentForm' },
+            'Hello, world! I am a CommentForm.'
+        );
+    }
+});
+
+var Comment = React.createClass({
+    displayName: 'Comment',
+    render: function render() {
+        return React.createElement(
+            'div',
+            { className: 'comment' },
+            React.createElement(
+                'h2',
+                { className: 'commentAuthor' },
+                this.props.author
+            ),
+            React.createElement('span', { dangerouslySetInnerHTML: this.rawMarkup() })
+        );
+    },
+    rawMarkup: function rawMarkup() {
+        var raw = marked(this.props.children.toString(), { sanitize: true });
+        return { __html: raw };
+    }
+});
+
+var CommentList = React.createClass({
+    displayName: 'CommentList',
+    render: function render() {
+        var commentNodes = this.props.data.map(function (comment) {
+            return React.createElement(
+                Comment,
+                { author: comment.author, key: comment.id },
+                comment.text
+            );
+        });
+        return React.createElement(
+            'div',
+            { className: 'commentList' },
+            commentNodes
+        );
+    }
+});
+
+var CommentBox = React.createClass({
+    displayName: 'CommentBox',
+    render: function render() {
+        return React.createElement(
+            'div',
+            { className: 'commentBox' },
+            React.createElement(
+                'h1',
+                null,
+                'Comments'
+            ),
+            React.createElement(CommentList, { data: this.props.data }),
+            React.createElement(CommentForm, null)
+        );
+    }
+});
+
+var data = [{ id: 1, author: "Hoge", text: "あああああ" }, { id: 2, author: "Fuga", text: "いいいいい" }];
+
+ReactDOM.render(React.createElement(CommentBox, { data: data }), document.getElementById('content'));
